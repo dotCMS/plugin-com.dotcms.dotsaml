dd0b15759347e1615536cc9ffdda1f5c6a5d398b
Jonathan <jsanca@Jonathans-MacBook-Pro-2.local>
2017-10-06 13:19:44 -0600

    #55 now when the login is done by dotcms, we invalidate the current session and renew with the previous attributes

---
+++ src/com/dotcms/plugin/saml/v3/filter/AutoLoginResult.java
@@ -0,0 +1,23 @@
+package com.dotcms.plugin.saml.v3.filter;
+
+import javax.servlet.http.HttpSession;
+
+public class AutoLoginResult {
+
+    private final HttpSession session;
+    private final boolean     autoLogin;
+
+    public AutoLoginResult(HttpSession session, boolean autoLogin) {
+
+        this.session = session;
+        this.autoLogin = autoLogin;
+    }
+
+    public HttpSession getSession() {
+        return session;
+    }
+
+    public boolean isAutoLogin() {
+        return autoLogin;
+    }
+}
