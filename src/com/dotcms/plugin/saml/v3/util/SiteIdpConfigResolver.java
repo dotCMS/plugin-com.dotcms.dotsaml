cc7fb516c0e4dd2ad4cbba435008a513b176a49e
Tom Stave <tstave@pmtghome.com>
2018-04-25 18:21:26 -0400

    Added looking for aliases in SiteIdpConfigResolver

--- src/com/dotcms/plugin/saml/v3/util/SiteIdpConfigResolver.java
+++ src/com/dotcms/plugin/saml/v3/util/SiteIdpConfigResolver.java
@@ -2,13 +2,17 @@ package com.dotcms.plugin.saml.v3.util;
 
 import com.dotcms.plugin.saml.v3.config.IdpConfig;
 import com.dotcms.plugin.saml.v3.config.IdpConfigHelper;
-
+import com.dotmarketing.beans.Host;
+import com.dotmarketing.business.APILocator;
 import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.exception.DotSecurityException;
 import com.dotmarketing.util.Logger;
+import com.dotmarketing.util.UtilMethods;
 import com.dotmarketing.util.json.JSONException;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -40,10 +44,65 @@ public class SiteIdpConfigResolver implements Serializable
 	 */
 	public IdpConfig resolveIdpConfig( final HttpServletRequest request ) throws DotDataException, IOException, JSONException
 	{
-		final String serverName = request.getServerName();
+		final String serverName = getHostId( request.getServerName() );
 		return this.resolveIdpConfig( serverName );
 	}
 
+	/*
+	 * Determines is the passed 'id' is in the 'hosts' as either a host or
+	 * alias.  If found in either field, the host string is returned.  This 
+	 * helps in determining a host name if an alias is passed.
+	 */
+	private String getHostId(String id) throws DotDataException {
+
+		// Does the ID have value
+		if (!UtilMethods.isSet(id)) {
+			throw new DotDataException("id is required.");
+		}
+
+		List<Host> hosts = null;
+		try {
+			hosts = APILocator.getHostAPI().findAll(APILocator.getUserAPI().getSystemUser(), false);
+
+		} catch (DotSecurityException e) {
+
+			throw new DotDataException("security exception while accessing getting host.");
+		}
+		
+		if ( hosts == null || hosts.isEmpty()){
+			throw new DotDataException("host data not found.");
+		}
+
+		String hostId = findHostId(id, hosts);
+
+		if (hostId == null) {
+			throw new DotDataException("id not found as host or alias.");
+		}
+
+		return hostId;
+	}
+
+	// Searches the host list and alias for each entry.
+	private String findHostId(String id, List<Host> hosts) {
+
+		for (Host h : hosts) {
+			if (id.equalsIgnoreCase(h.getHostname())) {
+				return h.getHostname();
+			}
+			if (h.getAliases() != null && !h.getAliases().isEmpty()) {
+
+				String[] aliases = h.getAliases().split("[\\s,]+");
+				for (String alias : aliases) {
+					if (id.equalsIgnoreCase(alias)) {
+						return h.getHostname();
+					}
+				}
+			}
+		}
+
+		return null;
+	}
+	
 	/**
 	 * Returns the IdpConfig associate to the current site, if it is not
 	 * found a match returns the default one site config.
