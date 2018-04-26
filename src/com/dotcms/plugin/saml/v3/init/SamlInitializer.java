38b71b5d2f1fdc8a87b55f3d0e99b902f3f5a8bf
Nathan (Ethode) <nathan@ethode.com>
2018-04-25 23:12:03 -0400

    Forcing cache build on initialize.

--- src/com/dotcms/plugin/saml/v3/init/SamlInitializer.java
+++ src/com/dotcms/plugin/saml/v3/init/SamlInitializer.java
@@ -1,5 +1,10 @@
 package com.dotcms.plugin.saml.v3.init;
 
+import com.dotcms.plugin.saml.v3.config.IdpConfigHelper;
+import com.dotmarketing.util.Logger;
+import com.dotmarketing.util.json.JSONException;
+
+import java.io.IOException;
 import java.security.Provider;
 import java.security.Security;
 import java.util.Map;
@@ -9,58 +14,71 @@ import org.opensaml.core.config.InitializationException;
 import org.opensaml.core.config.InitializationService;
 import org.opensaml.xmlsec.config.JavaCryptoValidationInitializer;
 
-import com.dotmarketing.util.Logger;
-
 /**
- * Default initializer
- * Responsibilities:
- *
- * - Init the Java Crypto.
- * - Init Saml Services.
- * - Init Plugin Configuration and meta data.
- *
+ * Default initializer Responsibilities: - Init the Java Crypto. - Init Saml
+ * Services. - Init Plugin Configuration and meta data.
  *
  * @author jsanca
  */
-public class SamlInitializer implements Initializer {
+public class SamlInitializer implements Initializer
+{
 
 	private static final long serialVersionUID = -5869927082029401479L;
-	
-	private final AtomicBoolean initDone = new AtomicBoolean(false);
 
-    public SamlInitializer(){
+	private final AtomicBoolean initDone = new AtomicBoolean( false );
 
-    }
+	public SamlInitializer()
+	{
 
-    @Override
-    public void init(final Map<String, Object> context) {
-    	
-    	JavaCryptoValidationInitializer javaCryptoValidationInitializer = new JavaCryptoValidationInitializer();
-        try {
-            javaCryptoValidationInitializer.init();
-        } catch (InitializationException e) {
-            e.printStackTrace();
-        }
+	}
 
-        for (Provider jceProvider : Security.getProviders()) {
-            Logger.info(this, jceProvider.getInfo());
-        }
+	@Override
+	public void init( final Map<String, Object> context )
+	{
+		JavaCryptoValidationInitializer javaCryptoValidationInitializer = new JavaCryptoValidationInitializer();
 
-        try {
-            Logger.info(this, "Initializing");
-            InitializationService.initialize();
-        } catch (InitializationException e) {
-            throw new RuntimeException("Initialization failed", e);
-        }
+		try
+		{
+			javaCryptoValidationInitializer.init();
+		}
+		catch ( InitializationException initializationException )
+		{
+			initializationException.printStackTrace();
+		}
 
-        this.initDone.set(true);
-    } 
+		for ( Provider jceProvider : Security.getProviders() )
+		{
+			Logger.info( this, jceProvider.getInfo() );
+		}
 
+		try
+		{
+			// Force a cache/file system read to initialize cache.
+			IdpConfigHelper.getInstance().getIdpConfigs();
+		}
+		catch ( JSONException | IOException exception )
+		{
+			Logger.error( this, "Could not initialize IdpConfigs.", exception );
+		}
 
-    @Override
-    public boolean isInitializationDone() {
+		try
+		{
+			Logger.info( this, "Initializing" );
+			InitializationService.initialize();
+		}
+		catch ( InitializationException e )
+		{
+			throw new RuntimeException( "Initialization failed", e );
+		}
 
-        return this.initDone.get();
-    } 
+		this.initDone.set( true );
+	}
+
+	@Override
+	public boolean isInitializationDone()
+	{
+
+		return this.initDone.get();
+	}
 
 }
