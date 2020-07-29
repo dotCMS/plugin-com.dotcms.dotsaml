d4060a88e5f87c28289fdadb0953ae466ec840b4
jdotcms <jonathan.sanchez@dotcms.com>
2020-07-29 16:30:39 -0600

    Added the support for http post

--- src/com/dotcms/plugin/saml/v3/service/DotHTTPPOSTDeflateEncoder.java
+++ src/com/dotcms/plugin/saml/v3/service/DotHTTPPOSTDeflateEncoder.java
@@ -1,5 +1,6 @@
 package com.dotcms.plugin.saml.v3.service;
 
+import com.dotmarketing.util.VelocityUtil;
 import org.opensaml.saml.saml2.binding.encoding.impl.HTTPPostEncoder;
 
 // migrated
@@ -7,5 +8,7 @@ public class DotHTTPPOSTDeflateEncoder extends HTTPPostEncoder {
 
 
     public DotHTTPPOSTDeflateEncoder() {
+        this.setVelocityTemplateId("/templates/saml2-post-binding.vm");
+        this.setVelocityEngine(VelocityUtil.getEngine());
     }
 }
